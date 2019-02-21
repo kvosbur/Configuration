4f4f6a288cebbbc94fc9617f0ea6abcc91ecf0b1
Kevin Vosburgh <kvosbur@purdue.edu>
2019-02-20 19:44:01 -0500

    Added basic script to get setup started

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#install vim
+echo "Installing vim"
+sudo apt install vim
+
+#install dependencies for ohmyzsh
+echo "Install zsh"
+sudo apt install zsh
+sudo apt install wget
+
+echo "Install Oh My Zsh"
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+echo "Change default shell to ZSH"
+chsh -s $(which zsh)
+
+#make soft links for config files
+echo "Making soft links to repository config files"
+ln -s ./zsrhc ../.zshrc
+ln -s ./vimrc ../.vimrc
+
+echo "Please logout and log back in for changes to take affect."
